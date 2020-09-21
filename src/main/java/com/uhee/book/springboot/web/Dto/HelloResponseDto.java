66d0a772b74d9f532a0977452edbc11d7e0bea5e
gynhyunkim <gyeonghyun.kimm@gmail.com>
2020-09-21 20:09:08 +0900

    Initial commit

---
+++ src/main/java/com/uhee/book/springboot/web/Dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.uhee.book.springboot.web.Dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
